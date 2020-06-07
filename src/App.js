3f768ef62492dce3ca453d845b1396c6bd2291de
Mufeez Khalid <zorawar.chowdhry@gmail.com>
2020-06-08 02:27:01 +0500

    change message again

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Hello World from all.
+         Hello World from all.....
         </p>
         {/* <a
           className="App-link"
