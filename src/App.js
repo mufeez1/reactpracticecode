be79629922126934ada2505c211d7fa8342fe2aa
Mufeez Khalid <zorawar.chowdhry@gmail.com>
2020-06-13 14:57:44 +0500

    Learn useState & css $ onClick $ tenenory operator

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Paragraph from './paragraph.js';
 
 import './App.css';
 
 function App() {
+  let [count,setcount]=useState(1);
+  let[isMorning,setMorning]=useState(true);
   return (
     // <h1> Hello World</h1>
-<div>
-<Paragraph country="pakistan" city="sialkot"/>
+<div className={`box ${isMorning ? 'dayLight':'NighLight'}`}>
+  <h2>Time : {isMorning?'morning':'night'}</h2>
+  <Paragraph counter={count}/>
+  <br/>
+  <button onClick={()=> setcount(++count)}>Add People</button>
+  <button onClick={()=> setMorning(!isMorning)}>Change Time</button>
+{/* <Paragraph country="pakistan" city="sialkot"/>
 <hr/>
 <Paragraph country="KSA" city="Makkah"/>
 <hr />
-<Paragraph country="Canada" city="Victoria"/>
-<hr />
+<Paragraph country="Australia" city="Victoria"/>
+<hr /> */}
 </div>  );
 }
 
--- src/paragraph.js
+++ src/paragraph.js
@@ -4,8 +4,8 @@ import './App.css';
 function Paragraph(props) {
     return (
         <div>
-      <h1> Hello {props.country}</h1>
-      <h1> Hello {props.city} from all</h1>
+      <h1> Hello world from {props.counter} people</h1>
+      {/* <h1> Hello {props.city} from all</h1> */}
       </div>
     );
   }
