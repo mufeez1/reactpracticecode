2a0a7e38146e19dfe0700cd9f03ffcbfd00434d0
Mufeez Khalid <zorawar.chowdhry@gmail.com>
2020-06-21 13:10:12 +0500

    update reducer

--- src/child2.js
+++ src/child2.js
@@ -8,6 +8,8 @@ const Child2 = ()=>{
         <h1> value of initial reducer is : {state}</h1>
         <h4>Using countReducer</h4>
 <button onClick={()=>dispatch('INCREMENT')}>Increment Reduce</button>
+
+<button onClick={()=>dispatch('DECREMENT')}>Decrement Reduce</button>
 </center>
         </div>
     )
