24b04191bfe7fdb5013236be4ef6250f431adda1
Mufeez Khalid <zorawar.chowdhry@gmail.com>
2020-06-29 15:28:15 +0500

    learn materialUI

--- src/App.js
+++ src/App.js
@@ -1,39 +1,51 @@
-import React, { useState, useEffect } from 'react';
-// import Parent from './parent.js';
-
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 import './App.css';
+// import { Grid } from '@material-ui/core';
 // import counterContext from './CounterContext.js';
+// import Parent from './parent.js';
 // import Child2 from './child2.js';
-
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 function App() {
-  let data = { title: "Waiting for Data" };
-  const [todo, setTodo] = useState(data);
-  // const [isData, setData] = useState(false);
-  const [isFetching, setFetching] = useState(false);
-  useEffect(() => {
-
-    async function fetchData() {
-      setFetching(true);
-      const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
-      console.log("Response =", response);
-      let data2 = await response.json();
-      setTodo(data2);
-      setFetching(false);
-      console.log("Data =", todo);
-      
-    }
-    fetchData();
-  });
-
-  if (isFetching) {
-    return <div>Data Loading.....</div>
-  }
-  return (
-    <div >
-      
-      <span>Title: {todo.title}</span>
+  const classes = useStyles();
+return(
+<div>
+<div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
     </div>
-  );
+<Button variant="outlined" color="primary">
+  Hello
+</Button>
+
+
+</div>
+);
 
 
 
@@ -47,6 +59,52 @@ function App() {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  //Fetch data in js through API
+  // let data = { title: "Waiting for Data" };
+  // const [todo, setTodo] = useState(data);
+  // // const [isData, setData] = useState(false);
+  // const [isFetching, setFetching] = useState(false);
+  // useEffect(() => {
+
+  //   async function fetchData() {
+  //     setFetching(true);
+  //     const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+  //     console.log("Response =", response);
+  //     let data2 = await response.json();
+  //     setTodo(data2);
+  //     setFetching(false);
+  //     console.log("Data =", todo);
+      
+  //   }
+  //   fetchData();
+  // });
+
+  // if (isFetching) {
+  //   return <div>Data Loading.....</div>
+  // }
+  // return (
+  //   <div >
+      
+  //     <span>Title: {todo.title}</span>
+  //   </div>
+  // );
+
+
+//context and reducer
   // let countState=useState(30)
   // //let [count, setCount]=useState(30)
   // return (
